34e026c61af04c235160462db754a32f84bdc131
griviala <78097561+griviala@users.noreply.github.com>
2023-03-21 17:25:17 +0300

    garpix_htmlmin==1.1.0

--- backend/htmlmin/setup.py
+++ backend/htmlmin/setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='garpix_htmlmin',
-    version='1.0.0',
+    version='1.1.0',
     description='',
     long_description=long_description,
     long_description_content_type='text/markdown',
